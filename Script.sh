e9739fab2d55b44f8743c68a40b239291aa8d016
Kali Linux <CyberGhost.jpg@gmail.com>
2022-08-28 23:45:08 -0300

    V3.01

--- Script.sh
+++ Script.sh
@@ -8,85 +8,7 @@ COL='\e[1;34m' #Color ${COL}
 NC='\033[0m' # No Color ${NC}
 ############ Estilos:
 #######
-# \033[0m Estilo normal \033[0m
-# \033[1m Negrita \033[0m
-# \033[2m Dim \033[0m
-# \033[4m Subrayado \033[0m
-# \033[5m Parpadeo \033[0m
-# \033[7m Inverso \033[0m
-# \033[8m Invisible \033[0m
-######
-############ Colores:
-######
-#Regular Colors
-#\033[0;30m  Black  \033[0m  # Black
-#\033[0;31m  Red    \033[0m  # Red
-#\033[0;32m  Green  \033[0m  # Green
-#\033[0;33m  Yellow \033[0m  # Yellow
-#\033[0;34m  Blue   \033[0m  # Blue
-#\033[0;35m  Purple \033[0m  # Purple
-#\033[0;36m  Purple \033[0m  # Cyan
-#\033[0;37m  White  \033[0m  # White
-
-#Bold
-#\033[1;30m  Black  \033[0m  # Black
-#\033[1;31m  Red    \033[0m  # Red
-#\033[1;32m  Green  \033[0m  # Green
-#\033[1;33m  Yellow \033[0m  # Yellow
-#\033[1;34m  Blue   \033[0m  # Blue
-#\033[1;35m  Purple \033[0m  # Purple
-#\033[1;36m  Purple \033[0m  # Cyan
-#\033[1;37m  White  \033[0m  # White
-
-#Underline
-#\033[4;30m  Black  \033[0m  # Black
-#\033[4;31m  Red    \033[0m  # Red
-#\033[4;32m  Green  \033[0m  # Green
-#\033[4;33m  Yellow \033[0m  # Yellow
-#\033[4;34m  Blue   \033[0m  # Blue
-#\033[4;35m  Purple \033[0m  # Purple
-#\033[4;36m  Purple \033[0m  # Cyan
-#\033[4;37m  White  \033[0m  # White
-
-#Background
-#\033[40m  Black  \033[0m    # Black
-#\033[41m  Red    \033[0m    # Red
-#\033[42m  Green  \033[0m    # Green
-#\033[43m  Yellow \033[0m    # Yellow
-#\033[44m  Blue   \033[0m    # Blue
-#\033[45m  Purple \033[0m    # Purple
-#\033[46m  Purple \033[0m    # Cyan
-#\033[47m  White  \033[0m    # White
-
-#High Intensity
-#\033[0;90m  Black  \033[0m  # Black
-#\033[0;91m  Red    \033[0m  # Red
-#\033[0;92m  Green  \033[0m  # Green
-#\033[0;93m  Yellow \033[0m  # Yellow
-#\033[0;94m  Blue   \033[0m  # Blue
-#\033[0;95m  Purple \033[0m  # Purple
-#\033[0;96m  Purple \033[0m  # Cyan
-#\033[0;97m  White  \033[0m  # White
-
-#Bold High Intensity
-#\033[1;90m  Black  \033[0m  # Black
-#\033[1;91m  Red    \033[0m  # Red
-#\033[1;92m  Green  \033[0m  # Green
-#\033[1;93m  Yellow \033[0m  # Yellow
-#\033[1;94m  Blue   \033[0m  # Blue
-#\033[1;95m  Purple \033[0m  # Purple
-#\033[1;96m  Purple \033[0m  # Cyan
-#\033[1;97m  White  \033[0m  # White
-
-#High Intensity backgrounds
-#\033[0;100m  Black  \033[0m  # Black
-#\033[0;101m  Red    \033[0m  # Red
-#\033[0;102m  Green  \033[0m  # Green
-#\033[0;103m  Yellow \033[0m  # Yellow
-#\033[0;104m  Blue   \033[0m  # Blue
-#\033[0;105m  Purple \033[0m  # Purple
-#\033[0;106m  Purple \033[0m  # Cyan
-#\033[0;107m  White  \033[0m  # White
+# En Colores.sh se encuentran todo los colores usados
 ######
 ############ To Do List:
 # ! Reparar la validacion de htop
@@ -139,13 +61,13 @@ while [ "$op" != 4 ]; do
             2) sleep 1s
                 last
                 sleep 1s
-                read -r -p "Presione enter para [ CONTINUAR ]..." op
+                read -r -p "Presione ${COL}enter${NC} para [ CONTINUAR ]..." op
                 [ "$op" = true ]
                 echo
             ;;
 
             3)
-                read -r -p"Presione enter para [ CONTINUAR ]..." op
+                read -r -p"Presione ${COL}enter${NC} para [ CONTINUAR ]..." op
                 [ "$op" = true ]
                 clear
             ;;
@@ -251,7 +173,7 @@ while [ "$op" != 4 ]; do
                         ;;
 
                         6)
-                            read -r -p "Presione enter para [ CONTINUAR ]..." op
+                            read -r -p "Presione ${COL}enter${NC} para [ CONTINUAR ]..." op
                             [ "$op" = true ]
                             clear
                         ;;
@@ -269,7 +191,7 @@ while [ "$op" != 4 ]; do
             ;;
 
             6) #Vuelve al inicio, puesto que al no haber una validación el while se vuelve nulo
-                read -r -p "Presione enter para [ CONTINUAR ]..." op
+                read -r -p "Presione ${COL}enter${NC} para [ CONTINUAR ]..." op
                 [ "$op" = true ]
                 clear
             ;;
@@ -321,7 +243,7 @@ while [ "$op" != 4 ]; do
             ;;
 
             3)
-                read -r -p "Presione enter para [ CONTINUAR ]..." op
+                read -r -p "Presione ${COL}enter${NC} para [ CONTINUAR ]..." op
                 [ "$op" = true ]
                 clear
             ;;
