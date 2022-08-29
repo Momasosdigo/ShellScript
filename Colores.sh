14f854f34d9972e4c028b0c3108d541ee34380cb
Kali Linux <CyberGhost.jpg@gmail.com>
2022-08-28 23:51:35 -0300

    Colores usados para el proyecto

--- Colores.sh
+++ Colores.sh
@@ -1,78 +1,77 @@
 #!/bin/bash
-
 echo
 echo "Regular Colors"
-echo -e "\033[0;30m  Black  \033[0m"  # Black
-echo -e "\033[0;31m  Red    \033[0m"  # Red
-echo -e "\033[0;32m  Green  \033[0m"  # Green
-echo -e "\033[0;33m  Yellow \033[0m"  # Yellow
-echo -e "\033[0;34m  Blue   \033[0m"  # Blue
-echo -e "\033[0;35m  Purple \033[0m"  # Purple
-echo -e "\033[0;36m  Purple \033[0m"  # Cyan
-echo -e "\033[0;37m  White  \033[0m"  # White
+echo -e "\033[0;30m Black  \033[0m"  # Black
+echo -e "\033[0;31m Red    \033[0m"  # Red
+echo -e "\033[0;32m Green  \033[0m"  # Green
+echo -e "\033[0;33m Yellow \033[0m"  # Yellow
+echo -e "\033[0;34m Blue   \033[0m"  # Blue
+echo -e "\033[0;35m Purple \033[0m"  # Purple
+echo -e "\033[0;36m Purple \033[0m"  # Cyan
+echo -e "\033[0;37m White  \033[0m"  # White
 
 echo
 echo "Bold"
-echo -e "\033[1;30m  Black  \033[0m"  # Black
-echo -e "\033[1;31m  Red    \033[0m"  # Red
-echo -e "\033[1;32m  Green  \033[0m"  # Green
-echo -e "\033[1;33m  Yellow \033[0m"  # Yellow
-echo -e "\033[1;34m  Blue   \033[0m"  # Blue
-echo -e "\033[1;35m  Purple \033[0m"  # Purple
-echo -e "\033[1;36m  Purple \033[0m"  # Cyan
-echo -e "\033[1;37m  White  \033[0m"  # White
+echo -e "\033[1;30m Black  \033[0m"  # Black
+echo -e "\033[1;31m Red    \033[0m"  # Red
+echo -e "\033[1;32m Green  \033[0m"  # Green
+echo -e "\033[1;33m Yellow \033[0m"  # Yellow
+echo -e "\033[1;34m Blue   \033[0m"  # Blue
+echo -e "\033[1;35m Purple \033[0m"  # Purple
+echo -e "\033[1;36m Purple \033[0m"  # Cyan
+echo -e "\033[1;37m White  \033[0m"  # White
 
 echo
 echo "Underline"
-echo -e "\033[4;30m  Black  \033[0m"  # Black
-echo -e "\033[4;31m  Red    \033[0m"  # Red
-echo -e "\033[4;32m  Green  \033[0m"  # Green
-echo -e "\033[4;33m  Yellow \033[0m"  # Yellow
-echo -e "\033[4;34m  Blue   \033[0m"  # Blue
-echo -e "\033[4;35m  Purple \033[0m"  # Purple
-echo -e "\033[4;36m  Purple \033[0m"  # Cyan
-echo -e "\033[4;37m  White  \033[0m"  # White
+echo -e "\033[4;30m Black  \033[0m"  # Black
+echo -e "\033[4;31m Red    \033[0m"  # Red
+echo -e "\033[4;32m Green  \033[0m"  # Green
+echo -e "\033[4;33m Yellow \033[0m"  # Yellow
+echo -e "\033[4;34m Blue   \033[0m"  # Blue
+echo -e "\033[4;35m Purple \033[0m"  # Purple
+echo -e "\033[4;36m Purple \033[0m"  # Cyan
+echo -e "\033[4;37m White  \033[0m"  # White
 
 echo
 echo "Background"
-echo -e "\033[40m  Black  \033[0m"    # Black
-echo -e "\033[41m  Red    \033[0m"    # Red
-echo -e "\033[42m  Green  \033[0m"    # Green
-echo -e "\033[43m  Yellow \033[0m"    # Yellow
-echo -e "\033[44m  Blue   \033[0m"    # Blue
-echo -e "\033[45m  Purple \033[0m"    # Purple
-echo -e "\033[46m  Purple \033[0m"    # Cyan
-echo -e "\033[47m  White  \033[0m"    # White
+echo -e "\033[40m Black  \033[0m"    # Black
+echo -e "\033[41m Red    \033[0m"    # Red
+echo -e "\033[42m Green  \033[0m"    # Green
+echo -e "\033[43m Yellow \033[0m"    # Yellow
+echo -e "\033[44m Blue   \033[0m"    # Blue
+echo -e "\033[45m Purple \033[0m"    # Purple
+echo -e "\033[46m Purple \033[0m"    # Cyan
+echo -e "\033[47m White  \033[0m"    # White
 
 echo
 echo "High Intensity"
-echo -e "\033[0;90m  Black  \033[0m"  # Black
-echo -e "\033[0;91m  Red    \033[0m"  # Red
-echo -e "\033[0;92m  Green  \033[0m"  # Green
-echo -e "\033[0;93m  Yellow \033[0m"  # Yellow
-echo -e "\033[0;94m  Blue   \033[0m"  # Blue
-echo -e "\033[0;95m  Purple \033[0m"  # Purple
-echo -e "\033[0;96m  Purple \033[0m"  # Cyan
-echo -e "\033[0;97m  White  \033[0m"  # White
+echo -e "\033[0;90m Black  \033[0m"  # Black
+echo -e "\033[0;91m Red    \033[0m"  # Red
+echo -e "\033[0;92m Green  \033[0m"  # Green
+echo -e "\033[0;93m Yellow \033[0m"  # Yellow
+echo -e "\033[0;94m Blue   \033[0m"  # Blue
+echo -e "\033[0;95m Purple \033[0m"  # Purple
+echo -e "\033[0;96m Purple \033[0m"  # Cyan
+echo -e "\033[0;97m White  \033[0m"  # White
 
 echo
 echo "Bold High Intensity"
-echo -e "\033[1;90m  Black  \033[0m"  # Black
-echo -e "\033[1;91m  Red    \033[0m"  # Red
-echo -e "\033[1;92m  Green  \033[0m"  # Green
-echo -e "\033[1;93m  Yellow \033[0m"  # Yellow
-echo -e "\033[1;94m  Blue   \033[0m"  # Blue
-echo -e "\033[1;95m  Purple \033[0m"  # Purple
-echo -e "\033[1;96m  Purple \033[0m"  # Cyan
-echo -e "\033[1;97m  White  \033[0m"  # White
+echo -e "\033[1;90m Black  \033[0m"  # Black
+echo -e "\033[1;91m Red    \033[0m"  # Red
+echo -e "\033[1;92m Green  \033[0m"  # Green
+echo -e "\033[1;93m Yellow \033[0m"  # Yellow
+echo -e "\033[1;94m Blue   \033[0m"  # Blue
+echo -e "\033[1;95m Purple \033[0m"  # Purple
+echo -e "\033[1;96m Purple \033[0m"  # Cyan
+echo -e "\033[1;97m White  \033[0m"  # White
 
 echo
 echo "High Intensity backgrounds"
-echo -e "\033[0;100m  Black  \033[0m"  # Black
-echo -e "\033[0;101m  Red    \033[0m"  # Red
-echo -e "\033[0;102m  Green  \033[0m"  # Green
-echo -e "\033[0;103m  Yellow \033[0m"  # Yellow
-echo -e "\033[0;104m  Blue   \033[0m"  # Blue
-echo -e "\033[0;105m  Purple \033[0m"  # Purple
-echo -e "\033[0;106m  Purple \033[0m"  # Cyan
-echo -e "\033[0;107m  White  \033[0m"  # White
+echo -e "\033[0;100m Black  \033[0m"  # Black
+echo -e "\033[0;101m Red    \033[0m"  # Red
+echo -e "\033[0;102m Green  \033[0m"  # Green
+echo -e "\033[0;103m Yellow \033[0m"  # Yellow
+echo -e "\033[0;104m Blue   \033[0m"  # Blue
+echo -e "\033[0;105m Purple \033[0m"  # Purple
+echo -e "\033[0;106m Purple \033[0m"  # Cyan
+echo -e "\033[0;107m White  \033[0m"  # White
