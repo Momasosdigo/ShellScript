04fe50cdc03c10521d38addd222a8d241887aee2
Kali Linux <CyberGhost.jpg@gmail.com>
2022-09-29 07:17:05 -0300

    Colores

--- Colores.sh
+++ Colores.sh
@@ -11,7 +11,7 @@ echo -e "\033[7m Inverso \033[0m"
 echo -e "\033[8m Invisible \033[0m"
 
 echo
-echo "Regular Colors"
+echo "Colors normales"
 echo -e "\033[0;30m Negro \033[0m" 
 echo -e "\033[0;31m Rojo \033[0m" 
 echo -e "\033[0;32m Verde \033[0m" 
@@ -22,7 +22,7 @@ echo -e "\033[0;36m Cian \033[0m"
 echo -e "\033[0;37m Blanco \033[0m" 
 
 echo
-echo "Bold"
+echo "Negrita"
 echo -e "\033[1;30m Negro \033[0m" 
 echo -e "\033[1;31m Rojo \033[0m" 
 echo -e "\033[1;32m Verde \033[0m" 
@@ -33,7 +33,7 @@ echo -e "\033[1;36m Cian \033[0m"
 echo -e "\033[1;37m Blanco \033[0m" 
 
 echo
-echo "Underline"
+echo "Subrayado"
 echo -e "\033[4;30m Negro \033[0m" 
 echo -e "\033[4;31m Rojo \033[0m" 
 echo -e "\033[4;32m Verde \033[0m" 
@@ -44,7 +44,7 @@ echo -e "\033[4;36m Cian \033[0m"
 echo -e "\033[4;37m Blanco \033[0m" 
 
 echo
-echo "Background"
+echo "Fondo"
 echo -e "\033[40m Negro \033[0m"   
 echo -e "\033[41m Rojo \033[0m"   
 echo -e "\033[42m Verde \033[0m"   
@@ -55,7 +55,7 @@ echo -e "\033[46m Cian \033[0m"
 echo -e "\033[47m Blanco \033[0m"   
 
 echo
-echo "High Intensity"
+echo "Alta intensidad"
 echo -e "\033[0;90m Negro \033[0m" 
 echo -e "\033[0;91m Rojo \033[0m" 
 echo -e "\033[0;92m Verde \033[0m" 
@@ -66,7 +66,7 @@ echo -e "\033[0;96m Cian \033[0m"
 echo -e "\033[0;97m Blanco \033[0m" 
 
 echo
-echo "Bold High Intensity"
+echo "Negrita con alta intensidad"
 echo -e "\033[1;90m Negro \033[0m" 
 echo -e "\033[1;91m Rojo \033[0m" 
 echo -e "\033[1;92m Verde \033[0m" 
@@ -77,7 +77,7 @@ echo -e "\033[1;96m Cian \033[0m"
 echo -e "\033[1;97m Blanco \033[0m" 
 
 echo
-echo "High Intensity backgrounds"
+echo "Fondo con alta intensidad"
 echo -e "\033[0;100m Negro \033[0m" 
 echo -e "\033[0;101m Rojo \033[0m" 
 echo -e "\033[0;102m Verde \033[0m" 
